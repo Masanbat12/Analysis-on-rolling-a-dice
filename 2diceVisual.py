902ca023d094ef61475f7ffb0b58f6020e613cce
masanbat <93978448+Masanbat12@users.noreply.github.com>
2024-02-13 20:51:18 +0200

    Update and rename 2dieVisual.py to 2diceVisual.py

--- 2diceVisual.py
+++ 2diceVisual.py
@@ -1,20 +1,20 @@
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
-from die import Die
+from dice import Dice
 
 # Creating 2 of D6 dice.
-die1 = Die()
-die2 = Die()
+dice1 = Dice()
+dice2 = Dice()
 
 # Making some results of rolling six-sided dice, and storing the results in a list.
 results = []
 for roll_num in range(2000):
-    result = die1.roll() + die2.roll()
+    result = dice1.roll() + dice2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-maxR = die1.num_sides + die2.num_sides
+maxR = dice1.num_sides + dice2.num_sides
 for value in range(2, maxR + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
